df1e65b2f68265c5672d237f0093a43a65abd646
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-06 20:02:12 +0200

    Update convert_icon.py

--- convert_icon.py
+++ convert_icon.py
@@ -3,12 +3,12 @@ from cairosvg import svg2png
 svg2png(
     url="icon.svg",
     write_to="src/assets/icon.png",
-    output_width="1024",
-    parent_height="1024",
+    output_width=1024,
+    parent_height=1024,
 )
 svg2png(
     url="icon.svg",
     write_to="src/assets/splash_android.png",
-    output_width="1152",
-    parent_height="1152",
+    output_width=1152,
+    parent_height=1152,
 )
