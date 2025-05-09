df90269d072e6fd9c4f533cddc0e63d3d44e54a3
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-09 06:39:06 +0200

    added video view

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import flet as ft
-from views import home
+from views import home, videoplayer
 
 
 def main(page: ft.Page):
@@ -7,43 +7,14 @@ def main(page: ft.Page):
         page.views.clear()
         if page.route == "/":
             page.views.append(home(adaptive=True, page=page))
-        elif page.route == "/portfolio":
-            page.views.append(
-                ft.View(
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    vertical_alignment=ft.MainAxisAlignment.CENTER,
-                    decoration=mydecoration(),
-                    bgcolor=bgcolor,
-                    route="/portfolio",
-                    appbar=myappbar(page=page),
-                    controls=[
-                        ft.SafeArea(
-                            ft.Column(
-                                [
-                                    ft.Text("Portfolio"),
-                                    mybutton(
-                                        text=FLASHLIGHTMSG(page=page),
-                                        on_click=lambda _: page.go("/flashlight"),
-                                        disabled=is_mobile(platform),
-                                        width=page.width * 0.5,
-                                    ),
-                                    mybutton(
-                                        text="Zurück",
-                                        on_click=lambda _: page.go("/"),
-                                        disabled=False,
-                                        width=page.width * 0.5,
-                                    ),
-                                ],
-                            )
-                        )
-                    ],
-                )
-            )
+        elif page.route == "/videoplayer":
+            page.views.append(videoplayer(adaptive=True, page=page))
 
         page.update()
 
     page.on_route_change = route_change
-    page.go("/")
+    # page.go("/")
+    page.go("/videoplayer")
 
 
 ft.app(main)
--- src/views/videoplayer.py
+++ src/views/videoplayer.py
@@ -1,5 +1,28 @@
 import flet as ft
+import flet_video as ftv
 
 
 def videoplayer(adaptive: bool, page: ft.Control):
-    return ft.View()
+    url = "https://cdn.media.ccc.de/congress/2024/webm-hd/38c3-198-eng-deu-pol-BlinkenCity_Radio-Controlling_Street_Lamps_and_Power_Plants_webm-hd.webm"
+    video_obj = [ftv.VideoMedia(url)]
+    return ft.View(
+        route="/videoplayer",
+        adaptive=True,
+        padding=0,
+        controls=[
+            video := ftv.Video(
+                expand=True,
+                playlist=video_obj[0],
+                playlist_mode=ftv.PlaylistMode.LOOP,
+                fill_color=ft.Colors.BLACK,
+                aspect_ratio=16 / 9,
+                volume=100,
+                autoplay=True,
+                filter_quality=ft.FilterQuality.HIGH,
+                muted=False,
+                on_loaded=lambda e: video.fu,
+                on_enter_fullscreen=lambda e: print("Video entered fullscreen!"),
+                on_exit_fullscreen=lambda e: print("Video exited fullscreen!"),
+            )
+        ],
+    )
